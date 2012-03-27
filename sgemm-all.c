7750570a744748bde7561b8c782cdcffddc24f7f
drgregory <grerobertsg@gmail.com>
2012-03-27 14:29:23 -0300

    Update sgemm-all.c

--- sgemm-all.c
+++ sgemm-all.c
@@ -35,7 +35,7 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 
     for (i = 0; i < n; i ++) {
 	for (j = 0; j < n/4*4; j += 4) {
-			
+
 	    x = _mm_loadu_ps(A + j + i*n);
 	    _MM_EXTRACT_FLOAT(temp, x, 0);
 	    At[i+j*n] = temp;
@@ -45,7 +45,7 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 	    At[i+(j+2)*n] = temp;
 	    _MM_EXTRACT_FLOAT(temp, x, 3);
 	    At[i+(j+3)*n] = temp;
-			
+
 	}
 	for (; j<n; j++) {
 	    At[i + j*n] = A[j+i*n];
@@ -69,7 +69,7 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 		partialSum5 = _mm_setzero_ps();
 		partialSum6 = _mm_setzero_ps();
 		partialSum7 = _mm_setzero_ps();
-	       
+
 		for(k = 0; k < n/4*4; k += 4) {
 		    a = _mm_loadu_ps(At + k + i*n);
 		    x = _mm_loadu_ps(B + k + j*n);
@@ -125,7 +125,7 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 			cij5 += At[k+(i+1)*n] * B[k+(j+1)*n];
 			cij6 += At[k+(i+2)*n] * B[k+(j+1)*n];
 			cij7 += At[k+(i+3)*n] * B[k+(j+1)*n];
-			
+
 		    }
 		    partialSum = _mm_set_ps(cij3, cij2, cij1, cij);
 		    partialSum1 = _mm_set_ps(cij7, cij6, cij5, cij4);
@@ -151,11 +151,11 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 		x = _mm_loadu_ps(At + k + (i+1)*n);
 		a = _mm_mul_ps(x, y);
 		partialSum1 = _mm_add_ps(partialSum1, a);
-		
+
 		x = _mm_loadu_ps(At + k + (i+2)*n);
 		a = _mm_mul_ps(x, y);
 		partialSum2 = _mm_add_ps(partialSum2, a);
-		
+
 		x = _mm_loadu_ps(At + k + (i+3)*n);
 		a = _mm_mul_ps(x, y);
 		partialSum3 = _mm_add_ps(partialSum3, a);
@@ -182,10 +182,124 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 
     // For each row i of A 
     for (;i < n; ++i) {
+    // For each column j of B 
+    for (j = 0; j < n/8*8; j+=8)
+    {
+      // Compute C(i,j)
 
+	cij = C[i+j*n];
+	cij1 = C[i+(j+1)*n];
+	cij2 = C[i+(j+2)*n];
+	cij3 = C[i+(j+3)*n];
+	cij4 = C[i+(j+4)*n];
+	cij5 = C[i+(j+5)*n];
+	cij6 = C[i+(j+6)*n];
+	cij7 = C[i+(j+7)*n];
+
+
+	//this will hold 4 floats which sum to the dot product
+	partialSum = _mm_setzero_ps();
+	partialSum1 = _mm_setzero_ps();
+	partialSum2 = _mm_setzero_ps();
+	partialSum3 = _mm_setzero_ps();
+	partialSum4 = _mm_setzero_ps();
+	partialSum5 = _mm_setzero_ps();
+	partialSum6 = _mm_setzero_ps();
+	partialSum7 = _mm_setzero_ps();
+
+      for(k = 0; k < n/4*4; k += 4) {
+	  x = _mm_loadu_ps(At + k + i*n);
+	  y = _mm_loadu_ps(B + k + j*n);
+	  d = _mm_mul_ps(x, y);
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+1) *n);
+	  a = _mm_mul_ps(x, y);
+	  partialSum = _mm_add_ps(partialSum, d);
+
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+2)*n);
+	  d = _mm_mul_ps(x, y);
+	  partialSum1 = _mm_add_ps(partialSum1, a);
+
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+3)*n);
+	  a = _mm_mul_ps(x, y);
+	  partialSum2 = _mm_add_ps(partialSum2, d);
+
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+4)*n);
+	  d = _mm_mul_ps(x, y);
+	  partialSum3 = _mm_add_ps(partialSum3, a);
+
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+5) *n);
+	  a = _mm_mul_ps(x, y);
+	  partialSum4 = _mm_add_ps(partialSum4, d);
+
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+6)*n);
+	  d = _mm_mul_ps(x, y);
+	  partialSum5 = _mm_add_ps(partialSum5, a);
+
+	  //accumulate dot prduct
+	  y = _mm_loadu_ps(B + k + (j+7)*n);
+	  a = _mm_mul_ps(x, y);
+	  partialSum6 = _mm_add_ps(partialSum6, d);
+
+	  //accumulate dot prduct
+	  partialSum7 = _mm_add_ps(partialSum7, a);
+
+      }
+
+			partialSum = _mm_hadd_ps(partialSum, partialSum1);
+			partialSum1 = _mm_hadd_ps(partialSum2, partialSum3);
+			partialSum2 = _mm_hadd_ps(partialSum, partialSum1); // [p0,p1,p2,p3] where p1 = accumulation of partialSum1
+
+			partialSum4 = _mm_hadd_ps(partialSum4, partialSum5);
+			partialSum5 = _mm_hadd_ps(partialSum6, partialSum7);
+			partialSum6 = _mm_hadd_ps(partialSum4, partialSum5); // [p4,p5,p6,p7]
+
+      _MM_EXTRACT_FLOAT(temp, partialSum2, 0);
+      cij += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum2, 1);
+      cij1 += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum2, 2);
+      cij2 += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum2, 3);
+      cij3 += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum6, 0);
+      cij4 += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum6, 1);
+      cij5 += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum6, 2);
+      cij6 += temp;
+      _MM_EXTRACT_FLOAT(temp, partialSum6, 3);
+      cij7 += temp;
+      
+			//cleanup k
+			for (; k < n; k ++) {
+			    cij += At[k+i*n] * B[k+j*n];
+			    cij1 += At[k+i*n] * B[k+(j+1)*n];
+			    cij2 += At[k+i*n] * B[k+(j+2)*n];
+			    cij3 += At[k+i*n] * B[k+(j+3)*n];
+			    cij4 += At[k+i*n] * B[k+(j+4)*n];
+			    cij5 += At[k+i*n] * B[k+(j+5)*n];
+			    cij6 += At[k+i*n] * B[k+(j+6)*n];
+			    cij7 += At[k+i*n] * B[k+(j+7)*n];
+			}
+			C[i+j*n] = cij;
+			C[i+(j+1)*n] = cij1;
+			C[i+(j+2)*n] = cij2;
+			C[i+(j+3)*n] = cij3;
+			C[i+(j+4)*n] = cij4;
+			C[i+(j+5)*n] = cij5;
+			C[i+(j+6)*n] = cij6;
+			C[i+(j+7)*n] = cij7;
+    }
+		//cleanup j
 		    // For each row i of A 
 
-    for (j=0;j < n/4*4; j+=4)
+    for (j = 0; j < n/4*4; j+=4)
     {
       // Compute C(i,j)
 
@@ -282,3 +396,4 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
   }
     free(At);
 }
+
