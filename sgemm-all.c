91c3f504e156d9332962aadcf5ca6d10cad87dac
drgregory <grerobertsg@gmail.com>
2012-03-27 22:45:15 -0300

    Update sgemm-all.c

--- sgemm-all.c
+++ sgemm-all.c
@@ -117,7 +117,7 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 		    cij5 = 0.0;
 		    cij6 = 0.0;
 		    cij7 = 0.0;
-		    for (; k < n/4*4; k +=4) {
+		    for (; k < n/2*2; k +=2) {
 			cij += At[k+i*n] * B[k+j*n];
 			cij1 += At[k+(i+1)*n] * B[k+j*n];
 			cij2 += At[k+(i+2)*n] * B[k+j*n];
@@ -135,24 +135,6 @@ void square_sgemm( int n, float *A, float *B, float *C ) {
 			cij5 += At[k+1+(i+1)*n] * B[k+1+(j+1)*n];
 			cij6 += At[k+1+(i+2)*n] * B[k+1+(j+1)*n];
 			cij7 += At[k+1+(i+3)*n] * B[k+1+(j+1)*n];
-			
-			cij += At[k+2+i*n] * B[k+2+j*n];
-			cij1 += At[k+2+(i+1)*n] * B[k+2+j*n];
-			cij2 += At[k+2+(i+2)*n] * B[k+2+j*n];
-			cij3 += At[k+2+(i+3)*n] * B[k+2+j*n];
-			cij4 += At[k+2+i*n] * B[k+2+(j+1)*n];
-			cij5 += At[k+2+(i+1)*n] * B[k+2+(j+1)*n];
-			cij6 += At[k+2+(i+2)*n] * B[k+2+(j+1)*n];
-			cij7 += At[k+2+(i+3)*n] * B[k+2+(j+1)*n];
-			
-			cij += At[k+3+i*n] * B[k+3+j*n];
-			cij1 += At[k+3+(i+1)*n] * B[k+3+j*n];
-			cij2 += At[k+3+(i+2)*n] * B[k+3+j*n];
-			cij3 += At[k+3+(i+3)*n] * B[k+3+j*n];
-			cij4 += At[k+3+i*n] * B[k+3+(j+1)*n];
-			cij5 += At[k+3+(i+1)*n] * B[k+3+(j+1)*n];
-			cij6 += At[k+3+(i+2)*n] * B[k+3+(j+1)*n];
-			cij7 += At[k+3+(i+3)*n] * B[k+3+(j+1)*n];
 			}
 			for(; k<n ; k ++) {
 			cij += At[k+i*n] * B[k+j*n];
